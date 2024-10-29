2ddefdc851ce70f66cd1eb1a703b5b17a39d1b56
Javier <javier.rojas.blum@gmail.com>
2024-10-29 16:07:36 +0000

    In Memory Authentication Provider

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -2,8 +2,8 @@ package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -11,7 +11,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT {
 
-    @WithMockUser("spring")
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
@@ -21,9 +28,9 @@ public class BeerControllerIT extends BaseIT {
     }
 
     @Test
-    void findBeersWithHttpBasic() throws Exception {
+    void findBeersWithAnonymous() throws Exception {
         mockMvc.perform(get("/beers/find")
-                        .with(httpBasic("spring", "guru")))
+                        .with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
