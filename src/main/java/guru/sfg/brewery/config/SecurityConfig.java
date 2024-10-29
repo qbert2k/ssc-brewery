26e5ee6fd984941ec92a40e5cebde396afdfaccb
Javier <javier.rojas.blum@gmail.com>
2024-10-29 16:18:26 +0000

    In Memory Authentication Provider

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -35,6 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("user")
                 .password("{noop}password")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("scott")
+                .password("{noop}tiger")
+                .roles("CUSTOMER");
     }
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -20,6 +20,14 @@ public class BeerControllerIT extends BaseIT {
     }
 
     @Test
+    void initCreationFormCustomer() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
                 .andExpect(status().isOk())
