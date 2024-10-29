2dcaa24b8642774e6311307e6e4ffb56a73cde05
Javier <javier.rojas.blum@gmail.com>
2024-10-29 15:01:30 +0000

    HTTP Method Matching

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -14,4 +14,10 @@ public class IndexControllerIT extends BaseIT {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void testGetBeersFind() throws Exception {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk());
+    }
 }
---
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,29 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/12345678-1234-1234-1234-12345678901"))
+                .andExpect(status().isOk());
+    }
+}
