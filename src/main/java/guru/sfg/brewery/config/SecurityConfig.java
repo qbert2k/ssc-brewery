1eb77718e8bae79183de3babb396cda365676fd8
Javier <javier.rojas.blum@gmail.com>
2024-12-03 20:08:41 +0000

    Database Authentication

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -62,27 +61,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("spring")
-                .password("{bcrypt}$2a$10$i6gvDcNdHQOockDScDYj3ecB9DkCStBhrR5cRip.knpRCr7OC7laS")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("{sha256}1efe6fbc76e266a77e38a60ef2758a30ab93eda94d335d477d32045675c2f146074aeb989a814be6")
-                .roles("USER");
-
-        auth.inMemoryAuthentication()
-                .withUser("customer")
-                .password("{ldap}{SSHA}ObPxaPc0ESvDvx7NxqEwL0oZTRnh2/Xp60uWyg==")
-                .roles("CUSTOMER");
-
-        auth.inMemoryAuthentication()
-                .withUser("provider")
-                .password("{bcrypt10}$2a$10$JDrD2kEvMIjcw9sNTsnZheNhwNGfZJbP.y9XSTpz8nmo71tDecKRu")
-                .roles("PROVIDER");
     }
 }
