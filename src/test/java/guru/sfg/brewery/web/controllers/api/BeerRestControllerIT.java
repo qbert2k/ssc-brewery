25e38440165f4516068d8466d6ed59157b6045e9
Javier <javier.rojas.blum@gmail.com>
2025-01-10 19:15:57 +0000

    User Roles: Refactor JUnit 5 Tests

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,9 +1,16 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.web.controllers.BaseIT;
-import org.junit.jupiter.api.Disabled;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -13,67 +20,51 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class BeerRestControllerIT extends BaseIT {
 
-    @Test
-    @Disabled
-    void deleteBeerUrlParameters() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .param("apiKey", "spring")
-                        .param("apiSecret", "guru"))
-                .andExpect(status().isOk());
-    }
+    @Autowired
+    BeerRepository beerRepository;
 
-    @Test
-    @Disabled
-    void deleteBeerUrlParametersBadCredentials() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .param("apiKey", "spring")
-                        .param("apiSecret", "guruXXXX"))
-                .andExpect(status().isUnauthorized());
-    }
+    @DisplayName("Delete Test")
+    @Nested
+    class DeleteTest {
 
-    @Test
-    @Disabled
-    void deleteBeerBadCredentials() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .header("Api-Key", "spring")
-                        .header("Api-Secret", "guruXXXX"))
-                .andExpect(status().isUnauthorized());
-    }
+        public Beer beerToDelete() {
+            Random rand = new Random();
 
-    @Test
-    @Disabled
-    void deleteBeer() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .header("Api-Key", "spring")
-                        .header("Api-Secret", "guru"))
-                .andExpect(status().isOk());
-    }
+            return beerRepository.saveAndFlush(Beer.builder()
+                    .beerName("Delete Me Beer")
+                    .beerStyle(BeerStyleEnum.IPA)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(String.valueOf(rand.nextInt(99999999)))
+                    .build());
+        }
 
-    @Test
-    void deleteBeerWithHttpBasic() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .with(httpBasic("spring", "guru")))
-                .andExpect(status().is2xxSuccessful());
-    }
+        @Test
+        void deleteBeerHttpBasic() throws Exception {
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("spring", "guru")))
+                    .andExpect(status().is2xxSuccessful());
+        }
 
-    @Test
-    void deleteBeerHttpBasicUserRole() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .with(httpBasic("user", "password")))
-                .andExpect(status().isForbidden());
-    }
+        @Test
+        void deleteBeerHttpBasicUserRole() throws Exception {
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("user", "password")))
+                    .andExpect(status().isForbidden());
+        }
 
-    @Test
-    void deleteBeerHttpBasicCustomerRole() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901")
-                        .with(httpBasic("scott", "tiger")))
-                .andExpect(status().isForbidden());
-    }
+        @Test
+        void deleteBeerHttpBasicCustomerRole() throws Exception {
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("scott", "tiger")))
+                    .andExpect(status().isForbidden());
+        }
 
-    @Test
-    void deleteBeerNoAuth() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/12345678-1234-1234-1234-12345678901"))
-                .andExpect(status().isUnauthorized());
+        @Test
+        void deleteBeerNoAuth() throws Exception {
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId()))
+                    .andExpect(status().isUnauthorized());
+        }
     }
 
     @Test
@@ -84,13 +75,22 @@ public class BeerRestControllerIT extends BaseIT {
 
     @Test
     void findBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/12345678-1234-1234-1234-12345678901"))
+        Beer beer = beerRepository.findAll().get(0);
+
+        mockMvc.perform(get("/api/v1/beer/" + beer.getId()))
                 .andExpect(status().isOk());
     }
 
     @Test
     void findBeerByUpc() throws Exception {
-        mockMvc.perform(get("/api/v1/beerUpc/12345678901"))
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerFormADMIN() throws Exception {
+        mockMvc.perform(get("/beers").param("beerName", "")
+                        .with(httpBasic("spring", "guru")))
                 .andExpect(status().isOk());
     }
 }
