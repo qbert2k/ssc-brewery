bec4d0304b39eb31edb2c615e1bef6de761af038
Javier <javier.rojas.blum@gmail.com>
2025-01-20 19:21:30 +0000

    User Roles: Security Expression Based Syntax

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -22,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                         .antMatchers(HttpMethod.GET, "/api/v1/beer/**")
                             .hasAnyRole("ADMIN", "CUSTOMER", "USER")
-                        .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
                         .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
                             .hasAnyRole("ADMIN", "CUSTOMER", "USER")
                         .mvcMatchers("/brewery/breweries")
