039a902c267e493f86ebaa4ce6c43cf0baa71918
Javier <javier.rojas.blum@gmail.com>
2025-01-21 20:38:59 +0000

    Multitenancy Security

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -1,21 +1,13 @@
 package guru.sfg.brewery.security;
 
-import guru.sfg.brewery.domain.security.Authority;
-import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,27 +21,7 @@ public class JpaUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.debug("Getting User info via JPA");
 
-        User user = userRepository.findByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User name: " + username));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                user.getEnabled(),
-                user.getAccountNonExpired(),
-                user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(),
-                convertToSpringAuthorities(user.getAuthorities()));
-    }
-
-    private Set<GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && !authorities.isEmpty()) {
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
     }
 }
