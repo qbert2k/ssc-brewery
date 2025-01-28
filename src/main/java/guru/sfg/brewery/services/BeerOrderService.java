1ea657988eac0ef79936f2c3cd6a5dbc10b598c6
Javier <javier.rojas.blum@gmail.com>
2025-01-28 19:06:29 +0000

    Multitenancy Security: Using Authentication Principle

--- src/main/java/guru/sfg/brewery/services/BeerOrderService.java
+++ src/main/java/guru/sfg/brewery/services/BeerOrderService.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Pageable;
 import java.util.UUID;
 
 public interface BeerOrderService {
+
     BeerOrderPagedList listOrders(UUID customerId, Pageable pageable);
+
+    BeerOrderPagedList listOrders(Pageable pageable);
 
     BeerOrderDto placeOrder(UUID customerId, BeerOrderDto beerOrderDto);
 
