359ebeb603cf884f3767f4839cd74c86f392b7c2
Javier <javier.rojas.blum@gmail.com>
2025-01-31 20:57:22 +0000

    Spring Security Event Publisher Logging of Authentication Success Events

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -1,9 +1,12 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,6 +16,19 @@ public class AuthenticationSuccessListener {
     @EventListener
     public void listenAuthenticationSuccessEvent(AuthenticationSuccessEvent event) {
         log.debug("User Logged In Okay");
+
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+            if (token.getPrincipal() instanceof User) {
+                User user = (User) token.getPrincipal();
+                log.debug("User name logged in: {}", user.getUsername());
+            }
+
+            if (token.getDetails() instanceof WebAuthenticationDetails) {
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+                log.debug("Source IP: {}", details.getRemoteAddress());
+            }
+        }
     }
 
     @EventListener
