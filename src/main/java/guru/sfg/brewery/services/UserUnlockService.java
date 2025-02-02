5bbe208f9b89529903113cb22ef13c05cbf56e0c
Javier <javier.rojas.blum@gmail.com>
2025-02-02 22:08:57 +0000

    Automatically Unlocking Accounts After Period

---
+++ src/main/java/guru/sfg/brewery/services/UserUnlockService.java
@@ -0,0 +1,36 @@
+package guru.sfg.brewery.services;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserUnlockService {
+
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
+    public void unlockAccounts() {
+        log.debug("Running Unlock Accounts");
+
+        List<User> lockedUsers = userRepository.findAllByAccountNonLockedAndLastModifiedDateIsBefore(
+                false,
+                Timestamp.valueOf(LocalDateTime.now().minusSeconds(30)));
+
+        if (!lockedUsers.isEmpty()) {
+            log.debug("Locked Account found {}, Unlocking...", lockedUsers.size());
+
+            lockedUsers.forEach(user -> user.setAccountNonLocked(true));
+            userRepository.saveAll(lockedUsers);
+        }
+    }
+}
