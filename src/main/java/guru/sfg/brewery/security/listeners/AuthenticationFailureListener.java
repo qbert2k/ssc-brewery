9c3f5e0db678dcf1cfd110f42d02511e4ab830fc
Javier <javier.rojas.blum@gmail.com>
2025-02-02 20:58:55 +0000

    Spring Security Event Publisher Logging of Authentication Failure Events

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,26 +1,25 @@
 package guru.sfg.brewery.security.listeners;
 
-import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class AuthenticationSuccessListener {
+public class AuthenticationFailureListener {
 
     @EventListener
-    public void listenAuthenticationSuccessEvent(AuthenticationSuccessEvent event) {
-        log.debug("User Logged In Okay");
+    public void listenAuthenticationFailureBadCredentialsEvent(AuthenticationFailureBadCredentialsEvent event) {
+        log.debug("Login failure");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
-            if (token.getPrincipal() instanceof User) {
-                User user = (User) token.getPrincipal();
-                log.debug("User name logged in: {}", user.getUsername());
+
+            if (token.getPrincipal() instanceof String) {
+                log.debug("Attempted username: {}", token.getPrincipal());
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
